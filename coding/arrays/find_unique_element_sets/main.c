321277fb1d0ca090adfd87918148429e24c9092f
Lokesh Chebrolu <lokeshch007@gmail.com>
2020-07-21 16:56:00 +0530

    removed stack usage from 'find_unique_element_sets' array problem

--- coding/arrays/find_unique_element_sets/main.c
+++ coding/arrays/find_unique_element_sets/main.c
@@ -1,5 +1,4 @@
 #include"quick_sort.h"
-#include"stack.h"
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -13,7 +12,7 @@ typedef struct array_sets
 
 void add_ele_to_array(array_sets_t *arr,int size,int ele)
 {
-	arr->set=(int*)realloc(arr->set,sizeof(int)*size);
+	arr->set=(int*)malloc(sizeof(int)*size);
 	if(!arr)
 	{
 		printf("relloc() error\n");
@@ -21,7 +20,13 @@ void add_ele_to_array(array_sets_t *arr,int size,int ele)
 	}
 
 	arr->setcount=size;
-	arr->set[size-1]=ele;
+	if(size>1)
+	{
+		for(int i=0;i<size;i++)
+			arr->set[i]=ele;
+	}
+	else
+		arr->set[0]=ele;
 }
 
 int main(void)
@@ -29,31 +34,22 @@ int main(void)
 	int array[10];
 	int size=10;
 
-	stack_init(size);
 	printf("Enter array: ");
 	for(int i=0;i<size;i++)
 		scanf("%d",array+i);
 
 	print_array(array,size);
-
 	quick_sort(array,size);
-
-	printf("\nSorted array:");
-	print_array(array,size);
 
 	int temp,sets=1;
 	for(int i=1;i<size;i++)
 	{
 		temp=array[i];
-		printf("%d\t",array[i-1]);
 		if(temp^array[i-1])
 		{
-			printf("\n");
 			sets++;
 		}
 	}
-	printf("%d\n",array[size-1]);
-
 
 	array_sets_t *sets_array=(array_sets_t*)malloc(sizeof(array_sets_t)*sets);
 	if(!sets_array)
@@ -76,32 +72,21 @@ int main(void)
 		{
 			if(same_ele_occur>0)
 			{
-				push(array[i]);
-				for(int j=0;j<=same_ele_occur;j++)
-				{
-					pop(&data);
-					add_ele_to_array(&sets_array[sets_array_index],j+1,data);
-					printf("\nsetcount:%d\n",sets_array[sets_array_index].setcount);
-				}
+				add_ele_to_array(&sets_array[sets_array_index],same_ele_occur+1,array[i]);
 				same_ele_occur=0;
 			}
-			add_ele_to_array(&sets_array[sets_array_index],1,array[i]);
+			else
+				add_ele_to_array(&sets_array[sets_array_index],1,array[i]);
 			sets_array_index++;
 		}
 		else
 		{
-			push(array[i]);
 			same_ele_occur++;
 		}
 	}
 	if(same_ele_occur>0)
 	{
-		push(array[size-1]);
-		for(int j=0;j<=same_ele_occur;j++)
-		{
-			pop(&data);
-			add_ele_to_array(&sets_array[sets_array_index],same_ele_occur+1,data);
-		}
+		add_ele_to_array(&sets_array[sets_array_index],same_ele_occur+1,array[size-1]);
 		same_ele_occur=0;
 	}
 	else
@@ -112,7 +97,7 @@ int main(void)
 	printf("\nUnique sets are : %d\n",sets);
 	for(int i=0;i<sets;i++)
 	{
-		printf("Set[%d] : ",i);
+		printf("Set[%d] : ",i+1);
 		for(int j=0;j<sets_array[i].setcount;j++)
 		{
 			printf("%d\t",sets_array[i].set[j]);
